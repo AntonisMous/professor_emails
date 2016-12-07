c983e057ab5a6f99d45a5c18a28dd73de76e3b79
AntonisMous <moustant21@gmail.com>
2016-12-07 15:08:34 +0200

    sending mails module

--- email_sending/send_emails.py
+++ email_sending/send_emails.py
@@ -1,34 +1,50 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-SMTP_SERVER = 'mail.netsteps.gr'
+SMTP_SERVER = 'mail.server'
 SMTP_PORT = 587
 
-user_name = 'a.m@novascientia.net'
-password = '$M1Aqzecx'
+user_name = 'my_mail'
+password = 'pswd'
 
-sender_address = 'a.m@novascientia.net'
-reciever_adress = 'a.m@novascientia.net'
+sender_address = 'my_mail'
+reciever_adress = 'your_mail'
 
 smtpObj = smtplib.SMTP(host=SMTP_SERVER, port=SMTP_PORT)
 smtpObj.starttls()
 smtpObj.login(user=user_name, password=password)
 
-reciever_name = 'Rene Woszidlo'
-reciever_work = 'Boeing'
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Link"
+msg['From'] = sender_address
+msg['To'] = reciever_adress
+
+html = """\
+<html>
+  <head></head>
+  <body>
+    <p>Hi!<br>
+       How are you?<br>
+       Here is the <a href="http://www.python.org">link</a> you wanted.
+    </p>
+  </body>
+</html>
+"""
+
+part2 = MIMEText(html, 'html')
+msg.attach(part2)
 
 
-subject = 'Subject: Nova-Patent\n'
-message_body = 'Dear ' + reciever_name
-try:
-    smtpObj.sendmail(from_addr=sender_address,
+smtpObj.sendmail(from_addr=sender_address,
                      to_addrs=reciever_adress,
-                     msg='Subject: Test!!!\nautomated mail')
-except:
-    try:
-        smtpObj.sendmail('a.m@novascientia.net', 'a.m@novascientia.net',
-                         'Subject: Test\nautomated mail')
-    except:
-        pass
+                     msg=msg.as_string())
+# except:
+#     try:
+#         smtpObj.sendmail('a.m@novascientia.net', 'a.m@novascientia.net',
+#                          'Subject: Test\nautomated mail')
+#     except:
+#         pass
 smtpObj.close()
 
 
